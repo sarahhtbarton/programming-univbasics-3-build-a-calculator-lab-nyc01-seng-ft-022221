335dea1135fb40c53afa40984631346fb27ddd99
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-10 01:31:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,7 +7,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  return num1 * num2
+  return num1 / num2
 end
 
 def multiplication(num1, num2)
@@ -15,9 +15,10 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-
+  return num1 % num2
 end
 
 def square_root(num)
-
+  #return num ** (1/2)
+  return Math.sqrt(num)
 end
